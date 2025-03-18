a82547b60e79fea599fa1475963d0cb63f453a18
Jack Wagner <wagjack@umich.edu>
2025-03-18 03:02:05 -0400

    transitional probability implemented + started evidence conditional probability

--- calculations.py
+++ calculations.py
@@ -1,9 +1,56 @@
-def transitional_probability():
-    pass
+from maze import check_valid_dim
+
+#Directions
+WEST = (0,-1)
+NORTH = (1,0)
+EAST = (0,1)
+SOUTH = (-1,0)
+
+DIRECTIONS = (WEST, NORTH, EAST, SOUTH)
+
+#Sensing Probabilities
+DO_OP = 0.95  #Detect object w/ object present
+NDO_OP = 0.05 #Not detect object w/ object present
+DO_NO = 0.15  #Detect object w/ no object present
+NDO_NO = 0.85 #Not detect object w/ no object present
+
+#Moving Probabilities
+STRAIGHT = 0.7
+DRIFT_LEFT = 0.1
+DRIFT_RIGHT = 0.2
 
 
-def evidence_conditional_probability():
-    pass
+def transitional_probability(state, maze, sense):
+    trans_prob = 1
+    
+    for d in range[3]:
+        nx = state[1] + DIRECTIONS[d][1]
+        ny = state[0] + DIRECTIONS[d][0]
+
+        if check_valid_dim(ny, nx):
+            surrounding = maze[ny][nx]
+            if sense[d] == 1:
+                if surrounding.probability == -1.0:
+                    trans_prob *= DO_OP
+                else:
+                    trans_prob *= NDO_OP
+            else:
+                if surrounding.probability != -1.0:
+                    trans_prob *= DO_NO
+                else:
+                    trans_prob *= NDO_NO
+
+    return trans_prob
+
+
+def evidence_conditional_probability(state, maze, move):
+    for d in DIRECTIONS:
+        nx = state[1] + d[1]
+        ny = state[0] + d[1]
+
+        if check_valid_dim(ny, nx):
+            surround = maze[ny][nx]
+            
 
 
 def filter(maze):
--- maze.py
+++ maze.py
@@ -47,36 +47,14 @@ class Maze:
             output += '\n' #print()
         return output
 
-"""
-def print_maze(maze):
-    for r in range(ROW):
-        for c in range(COL):
-            prob = maze[r][c].probability
-            if prob == -1.0:
-                print(' XXXX ', end='')
-            else:
-                round_prob = round(prob, 2)
-                if round_prob < 10.00:
-                    print(' ', end='')
-                print(f'{round_prob} ', end='')
-        print()
-"""
+def check_valid_dim(row, col):
+    return True if 0 < row < ROW and 0 < col < COL else False
 
 maze = ([False, False, True, True, False, False, True, True, False],
        [False, False, False, True, False, False, True, False, False],
        [True, True, True, True, True, True, True, True, True],
        [False, False, True, True, True, True, True, True, False],
        [False, False, False, True, False, False, True, False, False])
-
-"""
-def initialize_maze(maze):
-    for r in range(ROW):
-        for c in range(COL):
-            if maze[r][c]:
-                maze[r][c] = Location((c, r), 4.35)
-            else:
-                maze[r][c] = Location((c, r), -1.0)"
-"""
 
 m = Maze(maze)
 print(m)
