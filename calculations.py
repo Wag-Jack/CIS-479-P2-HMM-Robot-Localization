d3acc0f0e9121e73a333ef79583c921a747ee16e
Jack Wagner <wagjack@umich.edu>
2025-03-18 17:22:12 -0400

    further conditional probability implementation + main setup
    i do not know what i am doing <3

--- calculations.py
+++ calculations.py
@@ -6,13 +6,13 @@ NORTH = (1,0)
 EAST = (0,1)
 SOUTH = (-1,0)
 
-DIRECTIONS = (WEST, NORTH, EAST, SOUTH)
+DIRECTIONS = [WEST, NORTH, EAST, SOUTH]
 
 #Sensing Probabilities
-DO_OP = 0.95  #Detect object w/ object present
-NDO_OP = 0.05 #Not detect object w/ object present
-DO_NO = 0.15  #Detect object w/ no object present
-NDO_NO = 0.85 #Not detect object w/ no object present
+DO_OP = 0.95  #Detect obstacle w/ obstacle present
+NDO_OP = 0.05 #Not detect obstacle w/ obstacle present
+DO_NO = 0.15  #Detect obstacle w/ no obstacle present
+NDO_NO = 0.85 #Not detect obstacle w/ no obstacle present
 
 #Moving Probabilities
 STRAIGHT = 0.7
@@ -20,21 +20,21 @@ DRIFT_LEFT = 0.1
 DRIFT_RIGHT = 0.2
 
 
-def transitional_probability(state, maze, sense):
+def transitional_probability(state, maze, evidence):
     trans_prob = 1
     
-    for d in range[3]:
+    for d in range(3):
         nx = state[1] + DIRECTIONS[d][1]
         ny = state[0] + DIRECTIONS[d][0]
 
         if check_valid_dim(ny, nx):
             surrounding = maze[ny][nx]
-            if sense[d] == 1:
+            if evidence[d] == 1: #1 -> obstacle detected
                 if surrounding.probability == -1.0:
                     trans_prob *= DO_OP
                 else:
                     trans_prob *= NDO_OP
-            else:
+            else: #0 -> obstacle not detected
                 if surrounding.probability != -1.0:
                     trans_prob *= DO_NO
                 else:
@@ -44,18 +44,40 @@ def transitional_probability(state, maze, sense):
 
 
 def evidence_conditional_probability(state, maze, move):
-    for d in DIRECTIONS:
-        nx = state[1] + d[1]
-        ny = state[0] + d[1]
+    ev_cond_prob = 0
 
+    relative_directions = [EAST, SOUTH, WEST, NORTH]
+
+    for d in DIRECTIONS:
+
+        #Get coordinates of neighboring state for each direction
+        nx = state[1] + DIRECTIONS[d][1]
+        ny = state[0] + DIRECTIONS[d][0]
+
+        #Ensure valid location
         if check_valid_dim(ny, nx):
-            surround = maze[ny][nx]
+            #Determine neighbor's location
+            neighbor = maze[ny][nx]
+            
+            #Same direction non-obstacle does not add to
+            #evidence conditional probability
+            if d == move and neighbor.probability != -1.0:
+                pass 
+
+            #Determine the left and right directions
+            dir = DIRECTIONS.index(d)
+            left = DIRECTIONS[(dir - 1) % 4]
+            right = DIRECTIONS[(dir + 1) % 4]
+
+            #Determine what direction the neighbor will be moving
+            neighbor_move = relative_directions[dir]
             
 
 
-def filter(maze):
+
+def filter(maze, spaces, senses):
     pass
 
 
-def prediction(maze):
+def prediction(maze, spaces, move):
     pass
--- main.py
+++ main.py
@@ -13,12 +13,50 @@ S5 -> Z5 [0, 0, 0, 0]
 S6 -> Z6 [0, 0, 0, 0]
 """
 import maze as m
+import calculations as c
 
-maze = ([False, False, True, True, False, False, True, True, False],
+#Directions
+WEST = (0,-1)
+NORTH = (1,0)
+EAST = (0,1)
+SOUTH = (-1,0)
+
+evidence = [[1,0,1,1],
+          [0,0,1,1],
+          [0,0,0,1],
+          [0,1,0,0],
+          [0,0,0,0],
+          [0,0,0,0]]
+
+actions = [NORTH, EAST, NORTH, WEST, SOUTH]
+
+maze = [[False, False, True, True, False, False, True, True, False],
        [False, False, False, True, False, False, True, False, False],
        [True, True, True, True, True, True, True, True, True],
        [False, False, True, True, True, True, True, True, False],
-       [False, False, False, True, False, False, True, False, False])
+       [False, False, False, True, False, False, True, False, False]]
+
+open_squares = [(r.index(s), maze.index(r)) for r in maze for s in r if s == True]
+print(open_squares)
 
 ma = m.Maze(maze)
+
+for n in zip(evidence[:-1], actions):
+    #Evidence and Action respectively
+    ev = n[0]
+    ac = n[1]
+    
+    #Conduct filtering and print maze
+    ma = c.filter(ma, open_squares, ev)
+    print(f"Filtering after Evidence {ev}")
+    print(ma)
+
+    #Conduction prediction and print maze
+    ma = c.prediction(ma, open_squares, ac)
+    print(f"Prediction after Action {ac}")
+    print(ma)
+
+#Do the last filtering and print out the final maze
+ma = c.filter(ma, open_squares, evidence[5])
+print(f"Filtering after Evidence {ev}")
 print(ma)
